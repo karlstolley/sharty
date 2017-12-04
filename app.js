e02effc77da52cf8beef03b08aa2d937456f8abd
Karl Stolley <karl.stolley@gmail.com>
2017-12-04 11:37:58 -0600

    Set a default route if not set in YAML

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 // Using blocking readFileSync to make sure all URLs loaded
 let urls = yaml.safeLoad(fs.readFileSync('./urls.yaml'));
 
+// Check for a default route; set one if one does not exist
+if (typeof(urls.default) === 'undefined') {
+  urls.default = 'https://github.com/karlstolley/sharty'
+}
+
 app.get(/\/[a-z0-9-]+$/, (req, res) => {
   let short = req.url.substring(1); // trim initial slash
   let destination = urls[short];
