d49c4d5ac60313d05e167298b4177bea099379b7
Karl Stolley <karl.stolley@gmail.com>
2017-12-03 21:54:02 -0600

    Load up express

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
+const express = require('express');
+const app = express();
 
 // Using blocking readFileSync to make sure all URLs loaded
 let urls = yaml.safeLoad(fs.readFileSync('./urls.yaml'));
